aee878e3a5d9c961447ecfc262c1e51d9b2da48c
Jose Falcon <falcon@spotify.com>
2015-07-18 11:59:41 -0400

    Add leveldb for job persistence

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var debug = require('debug')('index');
 var fs = require('fs');
 var nconf = require('nconf');
 var Docker = require('./lib/docker');
+var level = require('level');
 var express = require('express');
 var uuid = require('uuid');
 var request = require('request');
@@ -18,7 +19,10 @@ var docker = new Docker({
   ca: fs.readFileSync(nconf.get('docker:ca'))
 });
 
-var jobs = {};
+var jobs = level(nconf.get('jobdb'), {
+  createIfMissing: true,
+  valueEncoding: 'json'
+});
 
 app.use(function requestLogger(req, res, next) {
   console.log(req.method + ' ' + req.url);
@@ -45,33 +49,42 @@ app.post('/api/v0/verify', function(req, res, next) {
   if (!git) return res.sendStatus(400);
 
   var jobId = uuid.v1();
-  var job = jobs[jobId] = {
+  var job = {
     id: jobId,
     url: '/api/v0/job/' + jobId,
     state: 'SUBMITTED',
     submitted: Date.now(),
   };
 
-  // respond asap.
-  res.status(202).json(job);
-
-  docker.buildImage(git, jobId, function(err, imageId) {
-    job.imageId = imageId;
-    docker.runImage(imageId, jobId, function(err, containerId) {
-      job.containerId = containerId;
-      job.state = 'RUNNING';
+  jobs.put(jobId, job, function(err) {
+    if (err) return res.statusCode(500);
+    // response asap.
+    res.status(202).json(job);
+    docker.buildImage(git, jobId, function(err, imageId) {
+      docker.runImage(imageId, jobId, function(err, containerId) {
+        job.imageId = imageId;
+        job.containerId = containerId;
+        job.state = 'RUNNING';
+        jobs.put(jobId, job, function(err) {
+          if (err) debug('Failed to update job state %s', jobId);
+        });
+      });
     });
   });
 });
 
 app.get('/api/v0/job/:id', function(req, res, next) {
   var id = req.params.id;
-  var job = jobs[req.params.id];
-  if (job) {
+  jobs.get(id, function(err, job) {
+    if (err) {
+      if (err.notFound) {
+        return res.sendStatis(404);
+      }
+      debug('Failed to fetch job %s', id);
+      return res.sendStatus(500);
+    }
     res.json(job);
-  } else {
-    res.sendStatus(404);
-  }
+  });
 });
 
 var dockerEmitter = docker.emitter();
@@ -82,17 +95,18 @@ dockerEmitter.start();
 dockerEmitter.on('die', function(message) {
   var containerId = message.id;
   docker.getContainerName(containerId, function(err, jobId) {
-    var job = jobs[jobId];
-    if (!job) return;
-
-    debug('Container finished executing %s for %s', containerId, jobId);
-    docker.readContainerLogs(containerId, function(err, log) {
-      var job = jobs[jobId];
-      job.result = log;
-      job.finished = Date.now();
-      job.state = 'FINISHED';
-
-      docker.removeContainer(containerId);
+    jobs.get(jobId, function(err, job) {
+      if (!job) return;
+      debug('Container finished executing %s for %s', containerId, jobId);
+      docker.readContainerLogs(containerId, function(err, log) {
+        docker.removeContainer(containerId);
+        job.result = log;
+        job.finished = Date.now();
+        job.state = 'FINISHED';
+        jobs.put(jobId, job, function(err) {
+          if (err) debug('Failed to update job state %s', jobId);
+        });
+      });
     });
   });
 });
